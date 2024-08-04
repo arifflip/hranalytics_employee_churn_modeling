9720562ef00e63c083d7be155d61112dc03a7534
arifflip <newarh14@gmail.com>
2024-08-04 10:29:54 +0700

    rm to renami it

--- script/00.custom_funtion.py
+++
@@ -1,9 +0,0 @@
-# Function to find records with outliers
-def find_outliers_pandas(data, column):
-	Q1 = data[column].quantile(0.25)
-	Q3 = data[column].quantile(0.75)
-	IQR = Q3 - Q1
-	lower_bound = Q1 - 1.5 * IQR
-	upper_bound = Q3 + 1.5 * IQR
-	outliers = data[(data[column] < lower_bound) | (data[column] > upper_bound)]
-	return outliers
