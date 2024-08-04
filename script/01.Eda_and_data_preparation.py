369a1dd533bda937f998f56324abfef3be108b4d
arifflip <newarh14@gmail.com>
2024-08-04 10:30:22 +0700

    add outlier checker

--- script/01.Eda_and_data_preparation.py
+++ script/01.Eda_and_data_preparation.py
@@ -12,10 +12,20 @@ import os
 data=pd.read_csv('data\Human_Resources.csv')
 
 ##----------------------------------------------------------
-## Checkin null
+## Data quality check
 ##----------------------------------------------------------
 
+#check data type
+data.dtypes
 
+#check missing value
+data.isnull().sum()
+
+#check central value
+data.describe().transpose()
+
+#check duplicated row
+data[data.duplicated()].transpose()
 
 
 
